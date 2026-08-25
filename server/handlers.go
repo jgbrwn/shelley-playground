9952f696f622e3ea68d0decc8327c0de893636ae
exe.dev user <exedev@jgbrwn-playground.exe.xyz>
2026-08-25 01:57:53 +0000

    Deploy: make systemd app-scoped and harden forklift
    Co-authored-by: Shelley <shelley@exe.dev>

--- server/handlers.go
+++ server/handlers.go
@@ -3562,8 +3562,13 @@ func (s *Server) handleGetSettings(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	redactPrivateSettings(settings)
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(settings)
+}
+
+func redactPrivateSettings(settings map[string]string) {
+	delete(settings, deployAPIKeySetting)
 }
 
 // handleSetSetting sets a single setting
